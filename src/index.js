6864d4f490ae95a6c2814ff13d4144cc04de5682
Jenia Romanenia <71782573+JeniaRomanenia@users.noreply.github.com>
2022-02-24 15:00:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+ module.exports = function check(str, bracketsConfig) {
+  let expected = [];
+  let ret = '';
+  let br;
+  let brackets = {};
+  bracketsConfig.map(_ => {
+    brackets[_[0]] = _[1];
+  });
+  br = str.split('');
+  br.map(item => {
+    console.log(item);
+    if (brackets[item]) {
+      if ((brackets[item] == '|' && expected[expected.length-1] == '|') || (brackets[item] == '7' && expected[expected.length-1] == '7') ||(brackets[item] == '8' && expected[expected.length-1] == '8'))  {
+        expected.pop();
+      } else {
+        expected.push(brackets[item]);
+
+      }
+      
+    } else {
+        if (expected.length == 0) {
+          expected.push('null');
+        } 
+        if (item === expected[expected.length-1]) {
+          expected.pop();
+      }
+    }
+  });
+  return expected.length == 0 ? true : false; 
 }
+
